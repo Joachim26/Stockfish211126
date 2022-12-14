aa18ef1a349b10adff39cf8321775c62796f278b
Joachim26 <69502420+Joachim26@users.noreply.github.com>
2022-12-15 00:47:01 +0100

    Ohne bench und -static

--- jni/Android.mk
+++ jni/Android.mk
@@ -14,5 +14,5 @@ ifeq ($(TARGET_ARCH_ABI), armeabi-v7a)
   LOCAL_CFLAGS += -DUSE_NEON=7 -DUSE_POPCNT
 endif
 
-LOCAL_LDFLAGS += -static -Wno-unused-command-line-argument
+LOCAL_LDFLAGS +=
 include $(BUILD_EXECUTABLE)
