f198cd881b5e923cb3886a5a81ef00e62d31497c
Joachim26 <69502420+Joachim26@users.noreply.github.com>
2023-04-29 22:04:50 +0200

    1000

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1072,7 +1072,7 @@ Value Eval::evaluate(const Position& pos) {
   else
   {
       int nnueComplexity;
-      int scale = 901 + pos.non_pawn_material() / 64;
+      int scale = 1100 + pos.non_pawn_material() / 64;
 
       Color stm = pos.side_to_move();
       Value optimism = pos.this_thread()->optimism[stm];
