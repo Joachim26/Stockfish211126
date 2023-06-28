f5bfce11c3e70723ceff247d974d7d8ea988194a
Muzhen Gaming <61100393+XInTheDark@users.noreply.github.com>
2023-06-28 18:36:11 +0800

    Simplify away improvement term in null move search
    passed STC:
    https://tests.stockfishchess.org/tests/view/649c0d2edc7002ce609d33b5
    LLR: 2.95 (-2.94,2.94) <-1.75,0.25>
    Total: 271104 W: 72181 L: 72217 D: 126706
    Ptnml(0-2): 691, 30042, 74129, 29992, 698
    
    passed LTC:
    https://tests.stockfishchess.org/tests/view/649d0dd7dc7002ce609d4efa
    LLR: 2.94 (-2.94,2.94) <-1.75,0.25>
    Total: 183120 W: 49469 L: 49418 D: 84233
    Ptnml(0-2): 84, 17636, 56063, 17699, 78
    
    closes https://github.com/official-stockfish/Stockfish/pull/4650
    
    Bench: 2642851

--- src/search.cpp
+++ src/search.cpp
@@ -789,7 +789,7 @@ namespace {
         && (ss-1)->statScore < 18404
         &&  eval >= beta
         &&  eval >= ss->staticEval
-        &&  ss->staticEval >= beta - 19 * depth - improvement / 13 + 257
+        &&  ss->staticEval >= beta - 20 * depth + 258
         && !excludedMove
         &&  pos.non_pawn_material(us)
         &&  ss->ply >= thisThread->nmpMinPly
