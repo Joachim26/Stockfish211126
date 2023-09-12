8354ca8ffcd8fc6ec2c3deb8174cbc814ab1cf55
Joachim26 <69502420+Joachim26@users.noreply.github.com>
2023-09-12 10:42:24 +0200

    Resolve conflict

--- src/benchmark.cpp
+++ src/benchmark.cpp
@@ -115,11 +115,11 @@ vector<string> setup_bench(const Position& current, istream& is) {
   string go, token;
 
   // Assign default values to missing arguments
-  string ttSize    = (is >> token) ? token : "16";
-  string threads   = (is >> token) ? token : "1";
-  string limit     = (is >> token) ? token : "13";
-  string fenFile   = (is >> token) ? token : "default";
-  string limitType = (is >> token) ? token : "depth";
+  std::string ttSize    = (is >> token) ? token : "16";
+  std::string threads   = (is >> token) ? token : "1";
+  std::string limit     = (is >> token) ? token : "13";
+  std::string fenFile   = (is >> token) ? token : "default";
+  std::string limitType = (is >> token) ? token : "depth";
   
   go = limitType == "eval" ? "eval" : "go " + limitType + " " + limit;
 
