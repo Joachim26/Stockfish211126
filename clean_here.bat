d3ae0d9cd3a18ec4490a1920eec2af6525bbe636
Joachim26 <69502420+Joachim26@users.noreply.github.com>
2023-11-21 18:40:26 +0100

    Upload rest of SFplusNPS

---
+++ clean_here.bat
@@ -0,0 +1,13 @@
+echo off
+del /s *.obj
+del /s *.ipch
+del /s *.db
+del /s *.iobj
+del /s *.ipdb
+del /s *.pdb
+del /s *.log
+del /s *.recipe
+del /s *.FileListAbsolute.txt
+del /s *.lastbuildstate
+del /s *.tlog
+del /s *.abcdef
