5585b2d7354aaa28190fdec3e7a454766e7464ab
Joachim26 <69502420+Joachim26@users.noreply.github.com>
2024-04-14 00:12:13 +0200

    Add medium.net to engine.h/.cpp

--- src/engine.cpp
+++ src/engine.cpp
@@ -50,6 +50,7 @@ Engine::Engine(std::string path) :
     states(new std::deque<StateInfo>(1)),
     networks(NN::Networks(
       NN::NetworkBig({EvalFileDefaultNameBig, "None", ""}, NN::EmbeddedNNUEType::BIG),
+      NN::NetworkMedium({EvalFileDefaultNameMedium, "None", ""}, NN::EmbeddedNNUEType::MEDIUM),
       NN::NetworkSmall({EvalFileDefaultNameSmall, "None", ""}, NN::EmbeddedNNUEType::SMALL))) {
     pos.set(StartFEN, false, &states->back());
 }
@@ -126,24 +127,31 @@ void Engine::set_ponderhit(bool b) { threads.main_manager()->ponder = b; }
 // network related
 
 void Engine::verify_networks() const {
-    networks.big.verify(options["EvalFile"]);
+    networks.big.verify(options["EvalFileBig"]);
+    networks.medium.verify(options["EvalFileMedium"]);
     networks.small.verify(options["EvalFileSmall"]);
 }
 
 void Engine::load_networks() {
-    networks.big.load(binaryDirectory, options["EvalFile"]);
+    networks.big.load(binaryDirectory, options["EvalFileBig"]);
+    networks.medium.load(binaryDirectory, options["EvalFileMedium"]);
     networks.small.load(binaryDirectory, options["EvalFileSmall"]);
 }
 
-void Engine::load_big_network(const std::string& file) { networks.big.load(binaryDirectory, file); }
-
+void Engine::load_big_network(const std::string& file) { 
+    networks.big.load(binaryDirectory, file);
+}
+void Engine::load_medium_network(const std::string& file) {
+    networks.medium.load(binaryDirectory, file);
+}
 void Engine::load_small_network(const std::string& file) {
     networks.small.load(binaryDirectory, file);
 }
 
 void Engine::save_network(const std::pair<std::optional<std::string>, std::string> files[2]) {
     networks.big.save(files[0].first);
-    networks.small.save(files[1].first);
+    networks.medium.save(files[1].first);
+    //networks.small.save(files[1].first); //comment out .medium. to save small
 }
 
 // utility functions
