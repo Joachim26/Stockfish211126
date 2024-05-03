a05ff4c8e34b38f541fc62ec5ea370f893c99f2b
Joachim26 <69502420+Joachim26@users.noreply.github.com>
2024-05-03 18:46:41 +0200

    SFNNv9.6.3 from SFNNv9.5.3 branch

--- src/evaluate.h
+++ src/evaluate.h
@@ -35,8 +35,8 @@ constexpr inline int SmallNetThreshold = 1274, PsqtOnlyThreshold = 2389;
 // for the build process (profile-build and fishtest) to work. Do not change the
 // name of the macro or the location where this macro is defined, as it is used
 // in the Makefile/Fishtest.
-#define EvalFileDefaultNameMedium "nn-ae6a388e4a1a.nnue"
-#define EvalFileDefaultNameBig "nn-a3d1bfca1672.nnue"
+#define EvalFileDefaultNameBig "nn-ae6a388e4a1a.nnue"
+#define EvalFileDefaultNameMedium "nn-a3d1bfca1672.nnue"
 #define EvalFileDefaultNameSmall "nn-baff1ede1f90.nnue"
 
 namespace NNUE {
