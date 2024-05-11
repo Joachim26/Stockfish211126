ecd84022878f481ffaf1f3670dc5064759cd5c29
Joachim26 <69502420+Joachim26@users.noreply.github.com>
2024-05-11 03:46:00 +0200

    Revert "only caches.big"
    This reverts commit 5e01b95cadf93481f3db61f24c1cc82be47e9bfb.

--- src/nnue/nnue_misc.cpp
+++ src/nnue/nnue_misc.cpp
@@ -50,7 +50,7 @@ void hint_common_parent_position(const Position&    pos,
     if (simpleEvalAbs > Eval::SmallNetThreshold)
         networks.small.hint_common_access(pos, &caches.small);
     else if (Stockfish::Eval::mediumNetOn)
-        networks.medium.hint_common_access(pos, &caches.big);
+        networks.medium.hint_common_access(pos, &caches.medium);
     else    
         networks.big.hint_common_access(pos, &caches.big);
 }
